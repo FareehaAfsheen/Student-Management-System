bd94d954a6a1970c632eff3d591027e349aac29c
FareehaAfsheen <fareehaafsheenchowdhury@gmail.com>
2026-09-18 14:41:36 +0600

    add person class

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hi It's me")
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def show_info(self):
+        print(f"Name: {self.name} \nAge : {self.age}") 
+
